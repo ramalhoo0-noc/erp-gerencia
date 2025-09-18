4515054440b7dde39440f5b1e9736bb349c7961c
Eric <ericramalho@hotmail.com>
2025-09-18 11:38:43 -0300

    Realizado e ajustado as telas de nova venda e novo cliente

--- script/src/vendas.js
+++ script/src/vendas.js
@@ -1,11 +1,18 @@
 const btnNovaVenda = document.getElementById("novaVenda");
 const btnFecharNovaVenda = document.getElementById("fecharModal");
-function fecharModal() {}
+const modalNovoCliente = document.getElementById("modalNovoCliente");
+const btnNovoCliente = document.getElementById("btnNovoCliente");
+
 btnNovaVenda.addEventListener("click", function () {
   console.log("botao clicado");
   const novaVenda = document.getElementById("modalVenda");
   novaVenda.style.display = "flex";
+
   btnFecharNovaVenda.addEventListener("click", function () {
     novaVenda.style.display = "none";
+  });
+
+  btnNovoCliente.addEventListener("click", function () {
+    modalNovoCliente.style.display = "flex";
   });
 });
